74f42176283fe7e5e8d5f50a0b84c89c51003b4d
LouisWangg <louisfernandoyantowang@gmail.com>
2022-05-30 01:39:18 +0700

    Update

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `https://detravel-database.herokuapp.com/`
+    baseURL: `http://localhost:8081/`
+    // http://localhost:8081/
+    // https://detravel-database.herokuapp.com/
   })
 }
