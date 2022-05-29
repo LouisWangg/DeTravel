a3d9b82b70aae56505a4e5f08314f8ccb8f57b4d
LouisWangg <louisfernandoyantowang@gmail.com>
2022-05-29 21:08:52 +0700

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -14,6 +14,7 @@ require('./routes') (app)
 
 sequelize.sync()
     .then(() => {
-        app.listen(config.port)
-        console.log(`Server stared on port ${config.port}`)
+        app.listen(process.env.PORT || 8081, () => {
+            console.log(`Server started on port ${PORT}`)
+        })
     })
