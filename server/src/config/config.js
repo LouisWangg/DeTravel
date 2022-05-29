99b2d1e4187661b4c9dc5b1aaf8a7003b2d8e67a
LouisWangg <louisfernandoyantowang@gmail.com>
2022-05-29 19:10:26 +0700

    Update

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,10 +1,12 @@
 module.exports = {
+    port: process.env.PORT || 8081,
     db: {
         options: {
             host: 'us-cdbr-east-05.cleardb.net',
-            database: 'heroku_fc1beaaf21c5a8c?',
+            port: '3306',
+            database: 'heroku_fc1beaaf21c5a8c',
             dialect: 'mysql',
-            user: 'b3aba3b2122df0',
+            username: 'b3aba3b2122df0',
             password: '3abb592a'
         }
     },
