73278cf5ebb023ee72cb1d44d64d330274846186
LouisWangg <louisfernandoyantowang@gmail.com>
2022-05-29 18:54:26 +0700

    Update

--- client/src/components/GoogleMap.vue
+++ client/src/components/GoogleMap.vue
@@ -10,7 +10,7 @@
               <v-icon>mdi-magnify</v-icon>
             </v-btn>
           </template>
-          <span>Ubah TEST</span>
+          <span>Ubah</span>
         </v-tooltip>
         <GmapMap :center="userPosition" :zoom='15' id ="map" style='width:100%; height:710px;' :options="{zoomControl: false, mapTypeControl: false}"> 
           <GmapMarker  
--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,12 +1,10 @@
 module.exports = {
-    port: process.env.PORT || 8081,
     db: {
         options: {
             host: 'us-cdbr-east-05.cleardb.net',
-            port: '3306',
             database: 'heroku_fc1beaaf21c5a8c?',
             dialect: 'mysql',
-            username: 'b3aba3b2122df0',
+            user: 'b3aba3b2122df0',
             password: '3abb592a'
         }
     },
